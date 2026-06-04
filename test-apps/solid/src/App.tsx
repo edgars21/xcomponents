e1aa52b3e5efc1cd082d8aca2fc8dd5c9cddb00d
Edgars Snepsts <edgars@snepsts.xyz>
2026-06-04 09:21:48 +0200

    deleted old started v2

--- test-apps/solid/src/App.tsx
+++ test-apps/solid/src/App.tsx
@@ -1,32 +1,9 @@
 // import Icon from "@xcomponents/icon";
-import Modal, { type Api } from "@xcomponents/modal";
+import {Button} from "@xcomponents/button";
 
 export default function App() {
-  let moadlApi: Api;
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        width: "100%",
-        height: "100%",
-        display: "grid",
-        "place-items": "center",
-      }}
-    >
-      <div style="display: flex; flex-gap: 10px">
-        Modal test
-        <button
-          onClick={() => {
-            moadlApi.open();
-          }}
-        >
-          Open Modal
-        </button>
-        <Modal ref={(api) => (moadlApi = api)}>
-          <h1>Inside the modal</h1>
-        </Modal>
-      </div>
-    </div>
+    <Button label="Click me" onClick={e => console.log("works: ", e)} />
   );
 }
